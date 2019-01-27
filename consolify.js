7fb36904706d1e891a5f051876630caee7b170da
Samson Radu <radusamson@ymail.com>
2019-01-27 19:31:17 +0200

    formatting

--- consolify.js
+++ consolify.js
@@ -4,50 +4,50 @@
  * @author Samson Radu
  */ 
 (function(){
-	var Consolify = {
-		init: function(cb){
-			if (typeof cb !== 'function'){
-				console.error("You must pass a callback function");
-				return false;
-			}
+    var Consolify = {
+        init: function(cb){
+            if (typeof cb !== 'function'){
+                console.error("You must pass a callback function");
+                return false;
+            }
 
-			const CATEGORY_INFO = "info";
-			const CATEGORY_WARN = "warn";
-			const CATEGORY_ERROR = "error";
+            const CATEGORY_INFO = "info";
+            const CATEGORY_WARN = "warn";
+            const CATEGORY_ERROR = "error";
 
-			let oLog = console.log;
-			let oWarn = console.warn;
-			let oErr = console.error;
+            let oLog = console.log;
+            let oWarn = console.warn;
+            let oErr = console.error;
 
-			console.log = console.info = function(){
-				if (oLog){
-					oLog.call(console, ...arguments);
-					cb(CATEGORY_INFO, arguments);
-				}
-			};
+            console.log = console.info = function(){
+                if (oLog){
+                    oLog.call(console, ...arguments);
+                    cb(CATEGORY_INFO, arguments);
+                }
+            };
 
-			console.warn = function(){
-				if (oWarn){
-					oWarn.call(console, ...arguments);
-					cb(CATEGORY_WARN, arguments);
-				}
-			};
+            console.warn = function(){
+                if (oWarn){
+                    oWarn.call(console, ...arguments);
+                    cb(CATEGORY_WARN, arguments);
+                }
+            };
 
-			console.error = function(){
-				if (oErr){
-					oErr.call(console, ...arguments);
-					cb(CATEGORY_ERROR, arguments);
-				}
-			};
-		}
-	}
+            console.error = function(){
+                if (oErr){
+                    oErr.call(console, ...arguments);
+                    cb(CATEGORY_ERROR, arguments);
+                }
+            };
+        }
+    }
 
-	//export
-	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined'){
-		module.exports = Consolify;
-	}
-	else {
-		window.Consolify = Consolify;
-	}
+    //export
+    if (typeof module !== 'undefined' && typeof module.exports !== 'undefined'){
+        module.exports = Consolify;
+    }
+    else {
+        window.Consolify = Consolify;
+    }
 
 })();
